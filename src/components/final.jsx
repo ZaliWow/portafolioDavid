ee50c7299279d55521f9136d18e8fd457170435e
David <morales.william@correounivalle.edu.co>
2024-04-09 21:30:54 -0500

    fix sizes

--- src/components/final.jsx
+++ src/components/final.jsx
@@ -6,8 +6,14 @@ export function Final({}) {
 
     return(
         <div className="boddyFinal">
+            <div className="finalCopy">
             <p>© 2024 William David Morales Pineda</p>
+            </div>
+            <div className="finalOptions">
+            <p>About Me | Experience | Skills | Projects | Education</p>
+            </div>
             
+                
         </div>
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -40,110 +40,109 @@ export function Navbar() {
   };
 
   return (
-    <AppBar position="fixed" elevation={0} sx={{backgroundColor:grey[100], color:'black'}}>
-      <Container maxWidth="xl" >
-        <Toolbar disableGutters >
-          <JavascriptIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'black',
-              textDecoration: 'none',
-            }}
-          >
-            DAVID MORALES  
-          </Typography>
+    <AppBar position="fixed"   sx={{backgroundColor: grey[100], color: 'black'}}>
+  <Container maxWidth="xl"  >
+    <Toolbar disableGutters>
+      <JavascriptIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+      <Typography
+        variant="h6"
+        noWrap
+        component="a"
+        href="#app-bar-with-responsive-menu"
+        sx={{
+          mr: 2,
+          display: { xs: 'none', md: 'flex' },
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'black',
+          textDecoration: 'none',
+        }}
+      >
+        DAVID MORALES
+      </Typography>
 
-          <Box sx={{flexGrow: 1, display: { xs: 'flex', md: 'none' } , justifyContent:'center' }}>
-            <IconButton
-            variant="dark"
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-             
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-         
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none', color:'black' },
-              }}
-            >
-             
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu} >
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-       
-            </Menu>
-          </Box>
-       
-          <JavascriptIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'black',
-              textDecoration: 'none',
-            }}
-          >
-            DAVID
-          </Typography>
-          <Box  sx={{ justifyContent: 'center' , flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'black', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+      <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'center' }}>
+        <IconButton
+          variant="dark"
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpenNavMenu}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{
+            display: { xs: 'block', md: 'none' },
+            color: 'black',
+          }}
+        >
+          {pages.map((page) => (
+            <MenuItem key={page} onClick={handleCloseNavMenu}>
+              <Typography textAlign="center">{page}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+        
+      </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Swap Mode">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <DarkMode sx={{color:'black'}}></DarkMode>
-              </IconButton>
-            </Tooltip>
-           
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+      <Typography
+        variant="h5"
+        noWrap
+        component="a"
+        href="#app-bar-with-responsive-menu"
+        sx={{
+          mr: 2,
+          display: { xs: 'flex', md: 'none' },
+          flexGrow: 1,
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'black',
+          textDecoration: 'none',
+        }}
+      >
+        DAVID
+      </Typography>
+
+      <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+      
+      </Box>
+
+      <Box sx={{ justifyContent: 'center', flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        {pages.map((page) => (
+          <Button
+            key={page}
+            onClick={handleCloseNavMenu}
+            sx={{ my: 2, color: 'black', display: 'block' }}
+          >
+            {page}
+          </Button>
+        ))}
+      </Box>
+       <Tooltip title="Swap Mode" >
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <DarkMode sx={{ color: 'black' }} />
+          </IconButton>
+        </Tooltip>
+    </Toolbar>
+  </Container>
+</AppBar>
+
   );
 }
